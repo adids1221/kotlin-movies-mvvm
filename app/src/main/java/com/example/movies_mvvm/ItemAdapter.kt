aebe98f8389e0a10635a9b3c2d01c904a99072fc
Adi <adids1221@gmail.com>
2023-03-30 19:48:55 +0300

    code refactor

--- app/src/main/java/com/example/movies_mvvm/ItemAdapter.kt
+++ app/src/main/java/com/example/movies_mvvm/ItemAdapter.kt
@@ -1,25 +1,22 @@
 package com.example.movies_mvvm
 
-import android.app.appsearch.BatchResultCallback
-import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.BatchingListUpdateCallback
-
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.movies_mvvm.databinding.ItemLayoutBinding
+import utils.getRating
 
 
-class ItemAdapter(private val items: List<Item>,val callback:ItemListener) :
+class ItemAdapter(private val items: List<Item>, val callback: ItemListener) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    interface ItemListener{
-        fun onItemClicked(index:Int)
+    interface ItemListener {
+        fun onItemClicked(index: Int)
     }
 
-   inner class ItemViewHolder(private val binding: ItemLayoutBinding) :
+    inner class ItemViewHolder(private val binding: ItemLayoutBinding) :
         RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
         fun bind(item: Item) {
@@ -32,39 +29,22 @@ class ItemAdapter(private val items: List<Item>,val callback:ItemListener) :
             binding.movieTitle.text = item.title
             binding.movieReleaseDate.text = item.releaseDate
             binding.itemRatingBar.rating = item.rating?.let { getRating(it) }!!
+        }
 
+
+        init {
+            binding.root.setOnClickListener(this)
+        }
+
+
+        override fun onClick(v: View?) {
+            callback.onItemClicked(adapterPosition)
 
 
         }
 
 
-
-
-        fun getRating(rating: Double): Float {
-            val maxRating = 5.0
-            val stepSize = 0.5
-            val numOfStars = 5
-
-            val numStars = (rating / (maxRating / numOfStars)).toFloat()
-
-            return (numStars * stepSize).toFloat()
-        }
-
-       init{
-           binding.root.setOnClickListener(this)
-       }
-
-
-
-       override fun onClick(v: View?) {
-           callback.onItemClicked(adapterPosition)
-
-
-
-       }
-
-
-   }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder =
         ItemViewHolder(
--- app/src/main/java/com/example/movies_mvvm/MovieFragment.kt
+++ app/src/main/java/com/example/movies_mvvm/MovieFragment.kt
@@ -6,12 +6,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.movies_mvvm.databinding.MovieLayoutBinding
+import utils.getRating
 
-class MovieFragment: Fragment() {
+class MovieFragment : Fragment() {
     private var _binding: MovieLayoutBinding? = null
     private val binding get() = _binding!!
 
@@ -23,31 +23,28 @@ class MovieFragment: Fragment() {
         _binding = MovieLayoutBinding.inflate(
             inflater, container, false
         )
-        val rotationImage :Animator = ObjectAnimator.ofFloat(binding.moviePoster,"rotationY",0f,360f).setDuration(2000)
+        val rotationImage: Animator =
+            ObjectAnimator.ofFloat(binding.moviePoster, "rotationY", 0f, 360f).setDuration(2000)
         rotationImage.start()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.getString("title")?.let{
+        arguments?.getString("title")?.let {
             binding.movieTitle.text = it
         }
-        arguments?.getString("releaseDate")?.let{
+        arguments?.getString("releaseDate")?.let {
             binding.movieReleaseDate.text = it
         }
-        arguments?.getString("description")?.let{
+        arguments?.getString("description")?.let {
             binding.movieDescription.text = it
         }
-        arguments?.getString("rating")?.let{ it ->
-
-
-
-
-
-
+        arguments?.getDouble("rating")?.let {
+            println("rating it -> $it")
+            binding.itemRatingBar.rating = getRating(it)
         }
-        arguments?.getString("poster")?.let{
+        arguments?.getString("poster")?.let {
             Glide
                 .with(binding.root)
                 .load(it)
